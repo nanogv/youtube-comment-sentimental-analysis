07c8b6f21b1009a9fc5d9553013d9e0c08ade3d7
nanogv <juanmanuelgarciavildoza@gmail.com>
2025-01-22 10:35:11 -0300

    Update YoutubeCommentScrapper.py

--- YoutubeCommentScrapper.py
+++ YoutubeCommentScrapper.py
@@ -9,7 +9,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # Replace with your own API key
-DEVELOPER_KEY = st.secrets["API_KEY"]
+DEVELOPER_KEY = st.secrets["AIzaSyAqzHzWTWOZgvMQlfoTGaEhThsQCOrT_3E"]
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 # Create a client object to interact with the YouTube API
